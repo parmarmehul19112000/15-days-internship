045cc612d2c6f56eb53ccf0bc48843ccb40b3cf7
parmarmehul19112000 <84672116+parmarmehul19112000@users.noreply.github.com>
2021-05-28 16:18:09 +0530

    Add files via upload

---
+++ day4/main.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Parmar Mehul</title>
+
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<?php
+include './theampart/menu.php';
+include './theampart/logo.php';
+?>
+
+
+<div id="wrapper"> 
+	<!-- end #header -->
+	<div id="page-bgtop"></div>
+	<div id="page">
+		<div><img src="images/pics01.jpg" width="920" height="300" alt="" /></div>
+		<div id="content">
+			<div class="post">
+				<h2 class="title"><a href="#">Welcome to Parmar Mehul </a></h2>
+				
+				<div style="clear: both;">&nbsp;</div>
+				<div class="entry">
+					hello, i am Parmar Mehul.
+				</div>
+			</div>
+			
+			<div style="clear: both;">&nbsp;</div>
+		</div>
+		<!-- end #content -->
+		<?php
+               include './theampart/sidebar.php';
+                ?>
+		<!-- end #sidebar -->
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<div id="page-bgbtm"></div>
+	<!-- end #page --> 
+</div>
+<?php
+               include './theampart/footer.php';
+                ?>
+<!-- end #footer -->
+</body>
+</html>
